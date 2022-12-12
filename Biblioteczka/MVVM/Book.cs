f52ef7f3740fc568e096db2aa222003dab913783
The-Mag-Pie <barteksor2000@onet.pl>
2022-12-12 20:22:27 +0100

    obiekty i struktury danych

--- Biblioteczka/MVVM/Book.cs
+++ Biblioteczka/MVVM/Book.cs
@@ -9,68 +9,14 @@ namespace Biblioteczka.MVVM
 {
     public class Book
     {
-        private long _id;
-        private string _title;
-        private string _author;
-        private string _description;
-        private string? _ebookLink;
-        private string? _audiobookLink;
-        private string? _movieLink;
-        private BitmapImage? _image = null;
-        private string _categoryName;
-
-        public long ID
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
-
-        public string Author
-        {
-            get { return _author; }
-            set { _author = value; }
-        }
-
-        public string Description
-        {
-            get { return _description; }
-            set { _description = value; }
-        }
-
-        public string? EbookLink
-        {
-            get { return _ebookLink; }
-            set { _ebookLink = value; }
-        }
-
-        public string? AudiobookLink
-        {
-            get { return _audiobookLink; }
-            set { _audiobookLink = value; }
-        }
-
-        public string? MovieLink
-        {
-            get { return _movieLink; }
-            set { _movieLink = value; }
-        }
-
-        public BitmapImage? Image
-        {
-            get { return _image; }
-            set { _image = value; }
-        }
-
-        public string CategoryName
-        {
-            get { return _categoryName; }
-            set { _categoryName = value; }
-        }
+        public long ID;
+        public string Title;
+        public string Author;
+        public string Description;
+        public string? EbookLink;
+        public string? AudiobookLink;
+        public string? MovieLink;
+        public BitmapImage? Image = null;
+        public string CategoryName;
     }
 }
